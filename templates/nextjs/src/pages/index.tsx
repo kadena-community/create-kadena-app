441d77cabe72e71ff78288c7e1578f600c3633e0
Mohamed Ismail <ismailreality1@gmail.com>
2023-11-16 14:07:58 +0100

    feat(ui): updated next js ui

--- templates/nextjs/src/pages/index.tsx
+++ templates/nextjs/src/pages/index.tsx
@@ -1,8 +1,10 @@
 import readMessage from '@/utils/readMessage';
 import writeMessage from '@/utils/writeMessage';
 import Head from 'next/head';
+import Image from 'next/image';
 import React, { useState } from 'react';
 import { SpinnerRoundFilled } from 'spinners-react';
+import KadenaImage from '../../public/assets/k-community-icon.png';
 import styles from '../styles/main.module.css';
 
 const Home: React.FC = (): JSX.Element => {
@@ -51,78 +53,121 @@ const Home: React.FC = (): JSX.Element => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.grid}>
-        <div className={styles.card}>
+        <section className={styles.headerWrapper}>
           <div className={styles.header}>
+            <Image
+              src={KadenaImage}
+              alt="Kadena Community Logo"
+              className={styles.logo}
+            />
             <h1 className={styles.title}>
-              Welcome to <span>Kadena!</span>
+              Start Interacting with the Kadena Blockchain
             </h1>
+            <p className={styles.note}>
+              This is the Kadena starter template using <strong>NextJS</strong>{' '}
+              to help you get started on your blockchain development.
+            </p>
+            <p className={styles.note}>
+              Use the form below to interact with the Kadena blockchain using{' '}
+              <em>
+                <code>@kadena/client</code>
+              </em>{' '}
+              and edit{' '}
+              <em>
+                <code>src/pages/index.tsx</code>
+              </em>{' '}
+              to get started.
+            </p>
           </div>
-          <p>
-            This is the Kadena starter template using nextjs to help you get
-            started on your blockchain development. Use the form below to
-            interact with the Kadena blockchain using{' '}
-            <code>@kadena/client</code> and edit{' '}
-            <code>src/pages/index.tsx</code> to get started.
-          </p>
-        </div>
-        <div className={styles.card}>
-          <h3 className={styles.cardTitle}>Interact with the blockchain</h3>
-          <section className={styles.cardSection}>
-            <label htmlFor="account" className={styles.fieldLabel}>
-              My Account
-            </label>
-            <input
-              id="account"
-              onChange={handleAccountInputChange}
-              value={account}
-              placeholder="Please enter a valid k:account"
-            ></input>
-          </section>
-          <section className={styles.cardSection}>
-            <label htmlFor="write-message" className={styles.fieldLabel}>
-              Write Message
-            </label>
-            <textarea
-              id="write-message"
-              onChange={handleWriteMessageInputChange}
-              value={messageToWrite}
-              disabled={writeInProgress}
-            ></textarea>
-            <button
-              onClick={handleWriteMessageClick}
-              disabled={
-                account === '' || messageToWrite === '' || writeInProgress
-              }
-            >
-              Write
-            </button>
-            {writeInProgress && (
-              <SpinnerRoundFilled size={30} color="#ed098f" />
-            )}
-          </section>
-          <section className={styles.cardSection}>
-            <label htmlFor="read-message" className={styles.fieldLabel}>
-              Read Message
-            </label>
-            <textarea
-              id="read-message"
-              disabled
-              value={messageFromChain}
-            ></textarea>
-            <button onClick={handleReadMessageClick} disabled={account === ''}>
-              Read
-            </button>
-          </section>
-        </div>
-        <div className={styles.card}>
-          <h3 className={styles.cardTitle}>Resources</h3>
-          <a href="https://docs.kadena.io/">
-            Find in-depth information about Kadena. &rarr;
-          </a>
-          <a href="https://github.com/kadena-community/kadena.js/tree/main/packages/tools/create-kadena-app/pact">
-            The smart contract powering this page. &rarr;
-          </a>
-        </div>
+        </section>
+        <section className={styles.contentWrapper}>
+          <div className={styles.blockChain}>
+            <div className={styles.card}>
+              <h4 className={styles.cardTitle}>Write to the blockchain</h4>
+              <fieldset className={styles.fieldset}>
+                <label htmlFor="account" className={styles.fieldLabel}>
+                  My Account
+                </label>
+                <input
+                  id="account"
+                  onChange={handleAccountInputChange}
+                  value={account}
+                  placeholder="Please enter a valid k:account"
+                  className={styles.input}
+                ></input>
+              </fieldset>
+              <fieldset className={styles.fieldset}>
+                <label htmlFor="write-message" className={styles.fieldLabel}>
+                  Write Message
+                </label>
+                <textarea
+                  id="write-message"
+                  onChange={handleWriteMessageInputChange}
+                  value={messageToWrite}
+                  disabled={writeInProgress}
+                  className={styles.input}
+                ></textarea>
+              </fieldset>
+              <div className={styles.buttonWrapper}>
+                {writeInProgress && (
+                  <SpinnerRoundFilled size={36} color="#ed098f" />
+                )}
+                <button
+                  onClick={handleWriteMessageClick}
+                  disabled={
+                    account === '' || messageToWrite === '' || writeInProgress
+                  }
+                  className={styles.button}
+                >
+                  Write
+                </button>
+              </div>
+            </div>
+            <div className={styles.card}>
+              <h4 className={styles.cardTitle}>Read from the blockchain</h4>
+              <fieldset className={styles.fieldset}>
+                <label htmlFor="read-message" className={styles.fieldLabel}>
+                  Read Message
+                </label>
+                <textarea
+                  id="read-message"
+                  disabled
+                  value={messageFromChain}
+                  className={styles.input}
+                ></textarea>
+              </fieldset>
+              <div className={styles.buttonWrapper}>
+                <button
+                  onClick={handleReadMessageClick}
+                  disabled={account === ''}
+                  className={styles.button}
+                >
+                  Read
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.helperSection}>
+            <div className={`${styles.card} ${styles.noBackground}`}>
+              <h4 className={styles.cardTitle}>Resources</h4>
+              <ul className={styles.list}>
+                <li>
+                  <a className={styles.link} href="https://docs.kadena.io/">
+                    Find in-depth information about Kadena. &rarr;
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className={styles.link}
+                    href="https://github.com/kadena-community/kadena.js/tree/main/packages/tools/create-kadena-app/pact"
+                  >
+                    The smart contract powering this page. &rarr;
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
